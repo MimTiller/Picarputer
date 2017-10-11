b198813577885afdf1bfbb4acff2e3ab8ce9c146
mimtiller <miller.allen.tim@gmail.com>
2017-10-10 19:35:09 -0500

    added test "controls for android AOA 2.0 accessory mode"

--- libs/audio.py
+++ libs/audio.py
@@ -6,24 +6,41 @@ import time
 import sys
 import logging
 from subprocess import call
+
+
 def connect(vendor,product):
 	dev = usb.core.find(idVendor=int(vendor, 16), idProduct=int(product, 16))
-	mesg = dev.ctrl_transfer(0xc0, 51, 0, 0, 2)
-	# here we should check if it returned version 2
-	print 'received mesg of %s'.format(mesg)
-	time.sleep(1)
-	# requesting audio
-	dev.ctrl_transfer(0x40, 0x3a, 1, 0, "")
-	print 'requested audio'
+	try:
+		mesg = dev.ctrl_transfer(0xc0, 51, 0, 0, 2)
+		# here we should check if it returned version 2
+		print 'received mesg of %s'.format(mesg)
+		time.sleep(1)
+		# requesting audio
+		dev.ctrl_transfer(0x40, 0x3a, 1, 0, "")
+		print 'requested audio'
 
-	# putting device in accessory mode
-	dev.ctrl_transfer(0x40, 53, 0, 0, "")
-	print 'put into accessory mode'
+		# putting device in accessory mode
+		dev.ctrl_transfer(0x40, 53, 0, 0, "")
+		print 'put into accessory mode'
+	except:
+		dev = usb.core.find(idVendor=int('18d1', 16), idProduct=int('2d02', 16))
+		mesg = dev.ctrl_transfer(0xc0, 51, 0, 0, 2)
+		# here we should check if it returned version 2
+		print 'received mesg of %s'.format(mesg)
+		time.sleep(1)
+		# requesting audio
+		dev.ctrl_transfer(0x40, 0x3a, 1, 0, "")
+		print 'requested audio'
 
+		# putting device in accessory mode
+		dev.ctrl_transfer(0x40, 53, 0, 0, "")
+		print 'put into accessory mode'
 	print 'finished script'
 	startaudio()
 	
 def startaudio():
 	print 'loading audio'
 	time.sleep(3)
-	call('pactl','load-module', 'module-loopback source=`pactl list sources short | grep alsa_input.usb | cut -f 1`')
+	call(('pactl','set-default-source','alsa_input.usb-motorola_XT1565_ZY2227GPNS-00.analog-stereo'))
+	
+
