8779b17847ea89b4e224a6b5e5f503ee768250cd
Tim Miller <tmiller@eosgsb.com>
2020-08-07 12:13:36 -0500

    Added new wallpapers, added autosizing to wallpaper widget

--- libs/settings.py
+++ libs/settings.py
@@ -1,20 +1,139 @@
 from kivy.core.window import Window
 from kivy.uix.popup import Popup
 from kivy.uix.spinner import Spinner
-from kivy.uix.settings import SettingsWithNoMenu, SettingOptions, SettingsWithSidebar, SettingItem
+from kivy.uix.settings import SettingsWithNoMenu, SettingOptions, SettingsWithSidebar
 from kivy.properties import NumericProperty, StringProperty, BooleanProperty, ListProperty, ObjectProperty
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.metrics import dp
 from kivy.uix.widget import Widget
 from kivy.uix.slider import Slider
 from kivy.uix.button import Button
 from kivy.uix.label import Label
+from kivy.compat import string_types, text_type
+from kivy.graphics import Color
 import importlib, os
 
 
 class SettingSpacer(Widget):
     # Internal class, not documented.
     pass
+
+class SettingItem(FloatLayout):
+
+    '''Base class for individual settings (within a panel). This class cannot
+    be used directly; it is used for implementing the other setting classes.
+    It builds a row with a title/description (left) and a setting control
+    (right).
+    Look at :class:`SettingBoolean`, :class:`SettingNumeric` and
+    :class:`SettingOptions` for usage examples.
+    :Events:
+        `on_release`
+            Fired when the item is touched and then released.
+    '''
+
+    title = StringProperty('<No title set>')
+    '''Title of the setting, defaults to '<No title set>'.
+    :attr:`title` is a :class:`~kivy.properties.StringProperty` and defaults to
+    '<No title set>'.
+    '''
+
+    desc = StringProperty(None, allownone=True)
+    '''Description of the setting, rendered on the line below the title.
+    :attr:`desc` is a :class:`~kivy.properties.StringProperty` and defaults to
+    None.
+    '''
+
+    disabled = BooleanProperty(False)
+    '''Indicate if this setting is disabled. If True, all touches on the
+    setting item will be discarded.
+    :attr:`disabled` is a :class:`~kivy.properties.BooleanProperty` and
+    defaults to False.
+    '''
+
+    section = StringProperty(None)
+    '''Section of the token inside the :class:`~kivy.config.ConfigParser`
+    instance.
+    :attr:`section` is a :class:`~kivy.properties.StringProperty` and defaults
+    to None.
+    '''
+
+    key = StringProperty(None)
+    '''Key of the token inside the :attr:`section` in the
+    :class:`~kivy.config.ConfigParser` instance.
+    :attr:`key` is a :class:`~kivy.properties.StringProperty` and defaults to
+    None.
+    '''
+
+    value = ObjectProperty(None)
+    '''Value of the token according to the :class:`~kivy.config.ConfigParser`
+    instance. Any change to this value will trigger a
+    :meth:`Settings.on_config_change` event.
+    :attr:`value` is an :class:`~kivy.properties.ObjectProperty` and defaults
+    to None.
+    '''
+
+    panel = ObjectProperty(None)
+    '''(internal) Reference to the SettingsPanel for this setting. You don't
+    need to use it.
+    :attr:`panel` is an :class:`~kivy.properties.ObjectProperty` and defaults
+    to None.
+    '''
+
+    content = ObjectProperty(None)
+    '''(internal) Reference to the widget that contains the real setting.
+    As soon as the content object is set, any further call to add_widget will
+    call the content.add_widget. This is automatically set.
+    :attr:`content` is an :class:`~kivy.properties.ObjectProperty` and defaults
+    to None.
+    '''
+
+    selected_alpha = NumericProperty(0)
+    '''(internal) Float value from 0 to 1, used to animate the background when
+    the user touches the item.
+    :attr:`selected_alpha` is a :class:`~kivy.properties.NumericProperty` and
+    defaults to 0.
+    '''
+
+    __events__ = ('on_release', )
+    def __init__(self, **kwargs):
+        super(SettingItem, self).__init__(**kwargs)
+        self.value = self.panel.get_value(self.section, self.key)
+
+
+    def add_widget(self, *largs):
+        if self.content is None:
+            return super(SettingItem, self).add_widget(*largs)
+        return self.content.add_widget(*largs)
+
+    def on_touch_down(self, touch):
+        if not self.collide_point(*touch.pos):
+            return
+        if self.disabled:
+            return
+        touch.grab(self)
+        self.selected_alpha = 1
+        return super(SettingItem, self).on_touch_down(touch)
+
+    def on_touch_up(self, touch):
+        if touch.grab_current is self:
+            touch.ungrab(self)
+            self.dispatch('on_release')
+            Animation(selected_alpha=0, d=.25, t='out_quad').start(self)
+            return True
+        return super(SettingItem, self).on_touch_up(touch)
+
+    def on_release(self):
+        pass
+
+    def on_value(self, instance, value):
+        if not self.section or not self.key:
+            return
+        # get current value in config
+        panel = self.panel
+        if not isinstance(value, string_types):
+            value = str(value)
+        panel.set_value(self.section, self.key, value)
 
 class SettingDynamicOptions(SettingOptions):
 	function_string = StringProperty()
@@ -78,5 +197,6 @@ class SettingSlider(SettingItem):
 class MySettings(SettingsWithSidebar):
 	def __init__(self,*args,**kargs):
 		super(MySettings,self).__init__(*args,**kargs)
+		Color=(0,0,0,0)
 		self.register_type('dynamic_options',SettingDynamicOptions)
 		self.register_type('slider',SettingSlider)
